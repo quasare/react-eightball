988f2dfef6d4d5efef748ea251e65d359319d688
Thomas Adams <tqsare@gmail.com>
2020-07-09 19:25:24 -0700

    add counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Eightball from './EightBall'
+import Counter from './Counter'
 
 
 
 function App() {
   return (
+    <>
+    <Counter />
     <Eightball/>
+    </>
   );
 }
 
